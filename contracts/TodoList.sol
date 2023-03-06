43c40bafa9114090bb710453ce38ca4ac49d51db
BerkeAnna <berke.anna0803@gmail.com>
2023-03-06 21:22:52 +0100

    34 min List tasks

--- contracts/TodoList.sol
+++ contracts/TodoList.sol
@@ -1,7 +1,23 @@
 // SPDX-License-Identifier: UNLICENSED
-pragma solidity >=0.4.22 <0.9.0;
+pragma solidity ^0.5.0;
 
 contract TodoList {
-    uint public taskCount = 0;
+    uint public taskCount=0;
 
+    struct Task{
+        uint id;
+        string content;
+        bool completed;
+    }
+
+    mapping (uint => Task) tasks;
+
+    constructor() public{
+        createTask("dasda");
+}
+
+    function createTask(string memory _content) public{
+        taskCount ++;
+        tasks[taskCount]= Task(taskCount, _content, false);
+    }
 }
